7a50176503c349c77e88bd63e4cfabd1245ce5b1
Killian Comerford <killiancomerford@outlook.com>
2020-02-08 12:44:46 +0100

    setup

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MonogamePhysics
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
